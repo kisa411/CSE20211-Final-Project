7eeba502658c8656db1422076aacfb5a21a38c9c
Emily Koh <kisa411@gmail.com>
2015-12-09 15:21:11 -0500

    First Draft

---
+++ final.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h> //for rand()
+#include <unistd.h> //for 'usleep function'
+#include "gfx5.h"
+#include <math.h>
+
+//function prototypes
+
+
+
+/*--------------------------------------------------------------------------------*/
+int main () {
+
+	gfx_open(700, 700, "Tamagotchi Borah & Emily");
+
+	return 0;
+
+}
+/*--------------------------------------------------------------------------------*/
+
+//
